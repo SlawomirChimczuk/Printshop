398043f85a1fa3e0bcbd5ba6501a49c2df61dd8b
Slawek <slawomirchim@outlook.com>
2021-03-22 23:44:54 +0100

    navigation buttons added

--- src/actions/showForm.js
+++ src/actions/showForm.js
@@ -2,7 +2,6 @@ import {DISPLAY_FORM} from './types'
 
 export const showFormHandle = () => {
     return (dispatch) => {
-        // console.log('działa')
         dispatch({
             type: DISPLAY_FORM
         })
